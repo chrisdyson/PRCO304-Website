c606f185a2146ac90fdf2a6f20d842497e97e609
chrisdyson <chris@dysonnet.co.uk>
2018-02-24 05:39:14 +0000

    results page
    added results page and updated all pages to support the new database design

--- app.js
+++ app.js
@@ -71,11 +71,12 @@ app.get('/averagesData', function (req, res) {
     var connection = mysql.createConnection({
         host: 'mydbttwebapp.cmv2uuxtdson.us-east-2.rds.amazonaws.com',
         user: 'chris',
-        password: 'mydbpass'
+        password: 'mydbpass',
+        database: 'TTdb'
     });
     connection.connect();
     var username = req.query.u;
-    connection.query("SELECT * FROM TTdb.user ORDER BY percentage DESC",
+    connection.query("SELECT * FROM user,teams,clubs WHERE user.teamID=teams.teamID AND teams.clubID = clubs.clubID ORDER BY percentage DESC",
         function (err, rows, fields) {
             if (!err) {
                 if (rows != '') {
@@ -88,15 +89,62 @@ app.get('/averagesData', function (req, res) {
                     res.write('<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>');
 
                     res.write('<table style="width: 80%;margin: 0 auto;" class="w3-table-all w3-card-4 w3-center">');
-                    res.write('<tr><th class="w3-center w3-hide-small" colspan="6" style="width:100%;background-color:#f1f1f1;border-bottom:1px solid #f1f1f1;padding-bottom:10px">Division 1</th><th class="w3-hide-medium w3-hide-large w3-center" colspan="5" style="width:100%;background-color:#f1f1f1;border-bottom:1px solid #f1f1f1;padding-bottom:10px">Division 1</th></tr>');
+                    res.write('<tr><th class="w3-center w3-hide-small" colspan="6" style="width:100%;background-color:#f1f1f1;padding-bottom:10px">Division 1</th><th class="w3-hide-medium w3-hide-large w3-center" colspan="5" style="width:100%;background-color:#f1f1f1;padding-bottom:10px">Division 1</th></tr>');
                     res.write('<tr><th class="w3-center" style="width:5%">#</th><th class="w3-center" style="width:19%">Name</th><th class="w3-center w3-hide-small" style="width:19%">Team</th><th class="w3-center" style="width:19%">Played</th><th class="w3-center" style="width:19%">Wins</th><th class="w3-center" style="width:19%">%</th></tr>');
-                    
+
                     for (i = 0; i < rows.length; i++) {
                         var percent = rows[i].percentage;
                         var percentConv = percent.toFixed(2);
                         var pos = i + 1;
-                        res.write('<tr><td class="w3-center">' + pos + '</td><td class="w3-center">' + rows[i].firstName + ' ' + rows[i].lastName + '</td><td class="w3-center w3-hide-small">' + rows[i].club + ' ' + rows[i].team + '</td><td class="w3-center">' + rows[i].gamesPlayed + '</td><td class="w3-center">' + rows[i].wins + '</td><td class="w3-center">' + percentConv + '%</td></tr>');
-                        
+                        res.write('<tr><td class="w3-center">' + pos + '</td><td class="w3-center">' + rows[i].firstName + ' ' + rows[i].lastName + '</td><td class="w3-center w3-hide-small">' + rows[i].clubName + ' ' + rows[i].teamName + '</td><td class="w3-center">' + rows[i].gamesPlayed + '</td><td class="w3-center">' + rows[i].wins + '</td><td class="w3-center">' + percentConv + '%</td></tr>');
+                    }
+                    res.write('</table>');
+                    res.end();
+                    connection.end();
+                } else { //error - no data
+                    console.log('Error while performing Query.');
+                    connection.end();
+                    res.write("Sorry, there was an error retrieving the user data");
+                    res.end();
+                }
+            } else { //error
+                console.log('Error while performing Query.');
+                connection.end();
+                res.write("Sorry, there was an error retrieving the user data");
+                res.end();
+            }
+        });
+});
+
+app.get('/resultsData', function (req, res) {
+    var connection = mysql.createConnection({
+        host: 'mydbttwebapp.cmv2uuxtdson.us-east-2.rds.amazonaws.com',
+        user: 'chris',
+        password: 'mydbpass',
+        database: 'TTdb'
+    });
+    connection.connect();
+    var username = req.query.u;
+    connection.query("SELECT * FROM teams,clubs WHERE teams.clubID = clubs.clubID ORDER BY points DESC",
+        function (err, rows, fields) {
+            if (!err) {
+                if (rows != '') {
+                    res.writeHead(200, {
+                        'Content-Type': 'text/html'
+                    });
+                    res.write('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">');
+                    res.write('<link rel="stylesheet" href="w3.css">');
+                    res.write('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">');
+                    res.write('<script src="http://code.jquery.com/jquery-latest.js" type="text/javascript"></script>');
+
+                    res.write('<table style="width: 80%;margin: 0 auto;" class="w3-table-all-fixtures w3-card-4 w3-center">');
+                    res.write('<tr><th class="w3-center" colspan="7" style="width:100%;background-color:#f1f1f1;padding-bottom:10px">Division 1</th></tr>');
+                    res.write('<tr style="background-color: #f1f1f1;" class="w3-hide-small"><th class="w3-center" style="width:5%">#</th><th class="w3-center" style="width:calc(95% / 6)%">Team</th><th class="w3-center" style="width:calc(95% / 6)%">Played</th><th class="w3-center" style="width:calc(95% / 6)%">Wins</th><th class="w3-center" style="width:calc(95% / 6)%">Draws</th><th class="w3-center" style="width:calc(95% / 6)%">Losses</th><th class="w3-center" style="width:calc(95% / 6)%">Points</th></tr>');
+                    res.write('<tr style="background-color: #f1f1f1;" class="w3-hide-medium w3-hide-large"><th class="w3-center" style="width:5%">#</th><th class="w3-center" style="width:calc(95% / 6)%">T</th><th class="w3-center" style="width:calc(95% / 6)%">P</th><th class="w3-center" style="width:calc(95% / 6)%">W</th><th class="w3-center" style="width:calc(95% / 6)%">D</th><th class="w3-center" style="width:calc(95% / 6)%">L</th><th class="w3-center" style="width:calc(95% / 6)%">Pts</th></tr>');
+                    
+                    for (i = 0; i < rows.length; i++) {
+                        var pos = i + 1;
+                        res.write('<tr><td class="w3-center">' + pos + '</td><td class="w3-center">' + rows[i].clubName + ' ' + rows[i].teamName + '</td><td class="w3-center">' + rows[i].teamGamesPlayed + '</td><td class="w3-center">' + rows[i].teamWins + '</td><td class="w3-center">' + rows[i].teamDraws + '</td><td class="w3-center">' + rows[i].teamLosses + '</td><td class="w3-center">' + rows[i].points + '</td></tr>');
                     }
                     res.write('</table>');
                     res.end();
@@ -120,11 +168,13 @@ app.get('/profileData', function (req, res) {
     var connection = mysql.createConnection({
         host: 'mydbttwebapp.cmv2uuxtdson.us-east-2.rds.amazonaws.com',
         user: 'chris',
-        password: 'mydbpass'
+        password: 'mydbpass',
+        database: 'TTdb'
     });
     connection.connect();
     var username = req.query.u;
-    connection.query("SELECT * FROM TTdb.user WHERE username = " + mysql.escape(req.query.u),
+
+    connection.query("SELECT * FROM user,teams,clubs WHERE user.teamID=teams.teamID AND teams.clubID = clubs.clubID AND user.username=" + mysql.escape(username),
         function (err, rows, fields) {
             if (!err) {
                 if (rows != '') {
@@ -152,18 +202,19 @@ app.get('/profileData', function (req, res) {
                         var percent = rows[i].percentage;
                         var percentConv = percent.toFixed(2);
                         res.write('<table style="width: 80%;margin: 0 auto;" class="w3-table-all w3-card-4 w3-center">');
-                        res.write('<tr><th class="w3-center" colspan="4" style="width:100%;background-color:#f1f1f1;border-bottom:1px solid #f1f1f1;padding-bottom:10px">' + rows[i].username + '</th></tr>');
-                        res.write('<tr><th class="w3-center" style="width:25%">Played</th><th class="w3-center" style="width:25%">Wins</th><th class="w3-center" style="width:25%">%</th><th class="w3-center" style="width:25%">Position</th></tr>');
-                        res.write('<tr><td class="w3-center">' + rows[i].gamesPlayed + '</td><td class="w3-center">' + rows[i].wins + '</td><td class="w3-center">' + percentConv + '%</td><td class="w3-center">23rd</td></tr>');
+                        res.write('<tr><th class="w3-center" colspan="3" style="width:100%;background-color:#f1f1f1;padding-bottom:10px">' + rows[i].username + '</th></tr>');
+                        res.write('<tr><th class="w3-center" style="width:33%">Played</th><th class="w3-center" style="width:33%">Wins</th><th class="w3-center" style="width:33%">Win %</th></tr>');
+                        res.write('<tr><td class="w3-center">' + rows[i].gamesPlayed + '</td><td class="w3-center">' + rows[i].wins + '</td><td class="w3-center">' + percentConv + '%</td></tr>');
                         res.write('</table>');
                         res.write('<br>');
                         res.write('<table style="width: 80%;margin: 0 auto;" class="w3-table-all w3-card-4 w3-center">');
-                        res.write('<tr><th class="w3-center" colspan="6" style="width:100%;background-color:#f1f1f1;border-bottom:1px solid #f1f1f1;padding-bottom:10px">' + rows[i].club + ' ' + rows[i].team + '</th></tr>');
-                        res.write('<tr class="w3-hide-small"><th class="w3-center" style="width: calc(100% / 6);">Played</th><th class="w3-center" style="width: calc(100% / 6);">Wins</th><th class="w3-center" style="width: calc(100% / 6);">Draws</th><th class="w3-center" style="width: calc(100% / 6);">Losses</th><th class="w3-center" style="width: calc(100% / 6);">Points</th><th class="w3-center" style="width: calc(100% / 6);">Position</th></tr>');
-                        res.write('<tr style="background-color: #f1f1f1;" class="w3-hide-large w3-hide-medium"><th class="w3-center" style="width: calc(100% / 6);">P</th><th class="w3-center" style="width: calc(100% / 6);">W</th><th class="w3-center" style="width: calc(100% / 6);">D</th><th class="w3-center" style="width: calc(100% / 6);">L</th><th class="w3-center" style="width: calc(100% / 6);">Pts</th><th class="w3-center" style="width: calc(100% / 6);">Pos</th></tr>');
-                        res.write('<tr style="background-color: #fff;"><td class="w3-center">19</td><td class="w3-center">4</td><td class="w3-center">3</td><td class="w3-center">12</td><td class="w3-center">61</td><td class="w3-center">12th</td></tr>');
+                        res.write('<tr><th class="w3-center" colspan="5" style="width:100%;background-color:#f1f1f1;padding-bottom:10px">' + rows[i].clubName + ' ' + rows[i].teamName + '</th></tr>');
+                        res.write('<tr class="w3-hide-small"><th class="w3-center" style="width: calc(100% / 5);">Played</th><th class="w3-center" style="width: calc(100% / 5);">Wins</th><th class="w3-center" style="width: calc(100% / 5);">Draws</th><th class="w3-center" style="width: calc(100% / 5);">Losses</th><th class="w3-center" style="width: calc(100% / 5);">Points</th></tr>');
+                        res.write('<tr style="background-color: #f1f1f1;" class="w3-hide-large w3-hide-medium"><th class="w3-center" style="width: calc(100% / 5);">P</th><th class="w3-center" style="width: calc(100% / 5);">W</th><th class="w3-center" style="width: calc(100% / 5);">D</th><th class="w3-center" style="width: calc(100% / 5);">L</th><th class="w3-center" style="width: calc(100% / 5);">Pts</th></tr>');
+                        res.write('<tr style="background-color: #fff;"><td class="w3-center">' + rows[i].teamGamesPlayed + '</td><td class="w3-center">' + rows[i].teamWins + '</td><td class="w3-center">' + rows[i].teamDraws + '</td><td class="w3-center">' + rows[i].teamLosses + '</td><td class="w3-center">' + rows[i].points + '</td></tr>');
                         res.write('</table>');
                     }
+
                     res.end();
                     connection.end();
                 } else { //error - no data
@@ -195,10 +246,11 @@ app.post('/editProfile', function (req, res) {
             var connection = mysql.createConnection({
                 host: 'mydbttwebapp.cmv2uuxtdson.us-east-2.rds.amazonaws.com',
                 user: 'chris',
-                password: 'mydbpass'
+                password: 'mydbpass',
+                database: 'TTdb'
             });
             connection.connect();
-            connection.query("UPDATE TTdb.user SET email = " + mysql.escape(post.email) + " WHERE username = " + mysql.escape(post.username),
+            connection.query("UPDATE user SET email = " + mysql.escape(post.email) + " WHERE username = " + mysql.escape(post.username),
                 function (err, rows, fields) {
                     if (!err) {
                         connection.end();
@@ -235,10 +287,11 @@ app.get('/venueData', function (req, res) {
     var connection = mysql.createConnection({
         host: 'mydbttwebapp.cmv2uuxtdson.us-east-2.rds.amazonaws.com',
         user: 'chris',
-        password: 'mydbpass'
+        password: 'mydbpass',
+        database: 'TTdb'
     });
     connection.connect();
-    connection.query("SELECT * FROM TTdb.clubs ORDER BY clubName;",
+    connection.query("SELECT * FROM clubs ORDER BY clubName;",
         function (err, rows, fields) {
             if (!err) {
                 if (rows != '') {
@@ -247,9 +300,9 @@ app.get('/venueData', function (req, res) {
                     });
                     res.write('<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">');
                     res.write('<link rel="stylesheet" href="w3.css">');
-                    res.write('<table style="width: 95%;margin: 0 auto;" class="w3-table-all w3-card-4"><tr><th style="width:50%">Club</th><th style="width:50%">Address</th></tr>');
+                    res.write('<table style="width: 95%;margin: 0 auto;" class="w3-table-all w3-card-4"><tr><th style="width:50%">Club</th><th style="width:50%">Location</th></tr>');
                     for (i = 0; i < rows.length; i++) {
-                        res.write('<tr><td>' + rows[i].clubName + '</td><td>' + rows[i].clubAddress + '</td></tr>');
+                        res.write('<tr><td>' + rows[i].clubName + '</td><td>' + rows[i].clubLocation + '</td></tr>');
                     }
                     res.write('</table>');
                     res.write('<br>')
@@ -261,10 +314,7 @@ app.get('/venueData', function (req, res) {
                     var j = 0;
                     for (; rows[j];) {
                         res.write('markers[' + j + '] = new google.maps.Marker({position: new google.maps.LatLng(' + rows[j].clubLat + ', ' + rows[j].clubLong + '),map: map});');
-                        if (rows[j].clubName == 'Saltash BEEC') {
-                            rows[j].clubName = 'Headquarters';
-                        }
-                        res.write('markers[' + j + '].index = ' + j + ';contents[' + j + '] = "' + rows[j].clubName + '";');
+                        res.write('markers[' + j + '].index = ' + j + ';contents[' + j + '] = "' + rows[j].clubMapLabel + '";');
                         res.write('infowindows[' + j + '] = new google.maps.InfoWindow({content: contents[' + j + '],maxWidth: 300});');
                         res.write('google.maps.event.addListener(markers[' + j + '], "click", function () {infowindows[this.index].open(map, markers[this.index]);map.panTo(markers[this.index].getPosition())});');
                         j++;
@@ -321,11 +371,12 @@ app.post('/login', function (req, res) {
             var connection = mysql.createConnection({
                 host: 'mydbttwebapp.cmv2uuxtdson.us-east-2.rds.amazonaws.com',
                 user: 'chris',
-                password: 'mydbpass'
+                password: 'mydbpass',
+                database: 'TTdb'
             });
 
             connection.connect();
-            connection.query("SELECT * FROM TTdb.user WHERE username = " + mysql.escape(post.username) + "AND password = " + mysql.escape(post.password),
+            connection.query("SELECT * FROM user WHERE username = " + mysql.escape(post.username) + "AND password = " + mysql.escape(post.password),
                 function (err, rows, fields) {
                     if (!err) {
                         if (rows != '') {
